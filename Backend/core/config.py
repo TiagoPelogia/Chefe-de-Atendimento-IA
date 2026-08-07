6b2e027fe19e3f1ab2fcbe0ffeeb58bd692c5977
TiagoPelogia <tiagopelogia@hotmail.com>
2026-08-06 21:35:47 -0300

    password hashing e JWT security

--- Backend/core/config.py
+++ Backend/core/config.py
@@ -5,6 +5,10 @@ class Settings(BaseSettings):
     database_url: str
     sql_echo: bool = False
 
+    jwt_secret_key: str
+    jwt_algorithm: str = "HS256"
+    access_token_expire_minutes: int = 15
+
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore",
